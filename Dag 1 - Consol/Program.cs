78cae44acffc76254a6ab35d74af712d80247098
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-16 12:32:44 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,7 +1,18 @@
-﻿Console.WriteLine("Hello World!");
+﻿//Console.WriteLine("Hello World!");
+//Console.WriteLine(" ");
+
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.WriteLine("You wrote your first lines of code.");
+//Console.Write("\n");
+
+//Console.WriteLine("Your message here");
+
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
 Console.WriteLine(" ");
 
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.WriteLine("You wrote your first lines of code.");
-Console.Write("\n");
+Console.Write("This is the first line.");
+Console.WriteLine(" ");
+Console.Write("This is the second line.");
