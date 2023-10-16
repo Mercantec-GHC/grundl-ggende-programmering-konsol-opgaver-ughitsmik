ddeb34f0fc1f2768068aba0215a5281e7445b256
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-16 14:06:58 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,3 +1,13 @@
-﻿string firstName;
-firstName = "Bob";
-Console.WriteLine(firstName);
+﻿string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.WriteLine(" ");
+Console.Write("You have ");
+Console.Write(messages);
+Console.WriteLine(" messages in your inbox.");
+Console.Write("The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
