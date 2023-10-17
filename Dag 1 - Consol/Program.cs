ab7f9dd7e29f5c2e21793afaff4464cd989e0bb5
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-17 08:19:25 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,9 +1,23 @@
-﻿string projectName = "ACME";
+﻿//string projectName = "ACME";
 
-string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
-string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+//string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+//string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
 
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+//string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
-Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+//Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+//Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+Console.WriteLine(" ");
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+Console.WriteLine(" ");
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
