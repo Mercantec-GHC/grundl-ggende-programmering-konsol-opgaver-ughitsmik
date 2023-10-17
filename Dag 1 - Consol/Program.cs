ace50674996c5b19572df06c02ecc5be2e088551
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-17 08:55:40 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -39,7 +39,14 @@
 //Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
 //Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
 
-int value1 = 3 + 4 * 5;
-int value2 = (3 + 4) * 5;
-Console.WriteLine(value1);
-Console.WriteLine(value2);
+//int value1 = 3 + 4 * 5;
+//int value2 = (3 + 4) * 5;
+//Console.WriteLine(value1);
+//Console.WriteLine(value2);
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
