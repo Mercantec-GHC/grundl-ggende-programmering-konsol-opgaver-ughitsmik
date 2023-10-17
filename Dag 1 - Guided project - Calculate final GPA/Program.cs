8cbac1e99d9d53324b61979b6f9da037ad459bdb
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-17 10:41:00 +0200

    Update Program.cs

--- Dag 1 - Guided project - Calculate final GPA/Program.cs
+++ Dag 1 - Guided project - Calculate final GPA/Program.cs
@@ -10,3 +10,36 @@ int course2Credit = 3;
 int course3Credit = 4;
 int course4Credit = 4;
 int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+
+int leadingDigit = (int)gradePointAverage;
+int trailingDigits = (int)(gradePointAverage * 100) - (leadingDigit * 100);
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t\t{course4Grade}\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t\t{course5Grade}\t{course5Credit}\n");
+Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
