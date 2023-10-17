3cc8ec2b433b4a3809ae24e2373893bd9c7a8dc2
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-17 08:31:19 +0200

    Update Program.cs

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -9,15 +9,37 @@
 //Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
 
 
-int firstNumber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNumber + secondNumber);
+//int firstNumber = 12;
+//int secondNumber = 7;
+//Console.WriteLine(firstNumber + secondNumber);
 
-string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
-Console.WriteLine(" ");
-Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
-Console.WriteLine(" ");
-Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+//string firstName = "Bob";
+//int widgetsSold = 7;
+//Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+//Console.WriteLine(" ");
+//Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+//Console.WriteLine(" ");
+//Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
 
+//int sum = 7 + 5;
+//int difference = 7 - 5;
+//int product = 7 * 5;
+//int quotient = 7 / 5;
+
+//Console.WriteLine("Sum: " + sum);
+//Console.WriteLine("Difference: " + difference);
+//Console.WriteLine("Product: " + product);
+//Console.WriteLine("Quotient: " + quotient);
+
+//int first = 7;
+//int second = 5;
+//decimal quotient = (decimal)first / (decimal)second;
+//Console.WriteLine(quotient);
+
+//Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+//Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
