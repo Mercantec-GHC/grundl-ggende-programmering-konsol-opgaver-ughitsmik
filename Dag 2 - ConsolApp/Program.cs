e1dd85f588e02b2b91c8d822f6465cf82f6dfecd
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-18 10:48:54 +0200

    Update Program.cs

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,31 +1,24 @@
-﻿//string[] names = { "Rowena", "Robin", "Bao" };
-//foreach (string name in names)
-//{
-//    Console.WriteLine(name);
-//}
+﻿//The following code does the following:
+//    1. Converts the original string to a char array
+//    2. Reverses the order of the chars, thus reversing the sentence
+//    3. Counts every "o"
+//    4. Prints out the reversed message and announces how many "o"'s are present 
 
-//int[] inventory = { 200, 450, 700, 175, 250 };
-//int sum = 0;
-//int bin = 0;
-//foreach (int items in inventory)
-//{
-//    sum += items;
-//    bin++;
-//    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-//}
-//Console.WriteLine($"We have {sum} items in inventory.");
+string str = "The quick brown fox jumps over the lazy dog.";
 
-//string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-//foreach (string orderID in orderIDs)
-//{
-//    Console.WriteLine(orderID);
-//}
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
 
-string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-foreach (string orderID in orderIDs)
+int x = 0;
+
+foreach (char i in charMessage)
 {
-    if (orderID.StartsWith("B"))
+    if (i == 'o')
     {
-        Console.WriteLine(orderID);
+        x++;
     }
 }
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
