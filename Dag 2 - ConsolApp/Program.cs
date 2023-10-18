2d9ae8ebb8d169f0a38da56e64482a14c90337db
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-18 08:06:39 +0200

    Update Program.cs

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,5 +1,27 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+﻿Random dice = new Random();
 
-Console.WriteLine(largerValue);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus points to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("Congratulations! You win!");
+}
+else 
+{
+    Console.WriteLine("Sorry, you lose.");
+}
