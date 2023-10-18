7f6f05ba2d4b06b639619919b99b66f448c4c916
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-18 10:00:25 +0200

    Update Program.cs

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,67 +1,23 @@
-﻿//Random dice = new Random();
-
-//int roll1 = dice.Next(1, 7);
-//int roll2 = dice.Next(1, 7);
-//int roll3 = dice.Next(1, 7);
-//int total = roll1 + roll2 + roll3;
-
-//Console.WriteLine($"DIce roll: {roll1} + {roll2} + {roll3} = {total}");
-
-//if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+﻿//string[] names = { "Rowena", "Robin", "Bao" };
+//foreach (string name in names)
 //{
-//    if ((roll1 == roll2) && (roll2 == roll3))
-//    {
-//            Console.WriteLine("You rolled triples! +6 bonus to total!");
-//    }
-//    else
-
-//    Console.WriteLine("You rolled doubles! +2 bonus points to total!");
-//    total += 2;
+//    Console.WriteLine(name);
 //}
 
-//if (total >= 16)
-//{
-//    Console.WriteLine("Congratulations! You win a new car!");
-//}
-//else if (total>=10) 
-//{
-//    Console.WriteLine("Congratulations! You win a new laptop!");
-//}
-//else if (total == 7)
-//{
-//    Console.WriteLine("Congratulations! You win a vacation trip for two!");
-//}
-//else
-//{
-//    Console.WriteLine("Congratulations! You win a kitten!");
-//}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] inventory = { 200, 450, 700, 175, 250 };
+        int sum = 0;
+        int bin = 0;
+        foreach (int items in inventory)
+        {
+            sum += items;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+        }
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-//Your code goes here
-
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+        Console.WriteLine($"We have {sum} items in inventory.");
+    }
 }
