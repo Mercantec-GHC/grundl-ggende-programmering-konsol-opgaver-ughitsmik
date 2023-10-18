8a5d1de6fdf2e620167d6d7b609d45c3b038a1d8
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-18 10:21:27 +0200

    Update Program.cs

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -4,20 +4,28 @@
 //    Console.WriteLine(name);
 //}
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        int[] inventory = { 200, 450, 700, 175, 250 };
-        int sum = 0;
-        int bin = 0;
-        foreach (int items in inventory)
-        {
-            sum += items;
-            bin++;
-            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-        }
+//int[] inventory = { 200, 450, 700, 175, 250 };
+//int sum = 0;
+//int bin = 0;
+//foreach (int items in inventory)
+//{
+//    sum += items;
+//    bin++;
+//    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+//}
+//Console.WriteLine($"We have {sum} items in inventory.");
 
-        Console.WriteLine($"We have {sum} items in inventory.");
+//string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+//foreach (string orderID in orderIDs)
+//{
+//    Console.WriteLine(orderID);
+//}
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
     }
 }
