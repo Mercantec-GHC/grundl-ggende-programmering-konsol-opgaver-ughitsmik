4c46e7f1536465129f3d21d5edec51f5aaa9e6fa
ughitsmik <148044221+ughitsmik@users.noreply.github.com>
2023-10-23 08:27:21 +0200

    Update Program.cs

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -1,35 +1,15 @@
-﻿//string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
 
-//decimal total = 0m;
-//string message = "";
+string[] message = pangram.Split(' ');
 
-//foreach (var value in values)
-//{
-//    decimal number; // stores the TryParse "out" value
-//    if (decimal.TryParse(value, out number))
-//    {
-//        total += number;
-//    }
-//    else
-//    {
-//        message += value;
-//    }
-//}
+string[] newMessage = new string[message.Length];
 
-//Console.WriteLine($"Message: {message}");
-//Console.WriteLine($"Total: {total}");
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
 
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-// The Convert class is best for converting the fractional decimal numbers into whole integer numbers
-// Convert.ToInt32() rounds up the way you would expect.
-int result1 = Convert.ToInt32((decimal)value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
